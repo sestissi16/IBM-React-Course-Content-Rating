cef829568b29d92629384b41af0ab7b1d5d94d27
Sari S <sestissi16@earlham.edu>
2025-03-17 11:12:23 -0700

    created basic component to be the item that you rate

---
+++ content-rating-state-practice/src/Components/RatableItem.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './RatableItem.css';
+
+class RatableItem extends Component {
+    constructor () {
+        super ();
+    }
+    render() {
+        return (
+            <div>
+            <h1>Item to Rate</h1>
+            </div>
+        );
+    }
+}
